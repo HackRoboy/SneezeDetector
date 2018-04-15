d042fd6794aec0004f2c6a33726f885de2c1a5be
DerGenaue <dergenaue@online.de>
2018-04-15 13:48:58 +0200

    Hotword detector skeleton

---
+++ detectHotword.py
@@ -0,0 +1,17 @@
+
+
+
+
+
+def get_distance(mfcc1, mfcc2):
+    return 0.0
+
+
+def get_avg_distance(mfccs):
+    sum = 0.0
+    n = 0
+    for mfcc1 in mfccs:
+        for mfcc2 in mfccs:
+            sum += get_distance(mfcc1, mfcc2)
+            n += 1
+    return sum/n
